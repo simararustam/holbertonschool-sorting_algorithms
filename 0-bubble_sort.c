45bbe2a374f4038c1cf410421ff2c7df403d5755
simararustam <simararustam@gmail.com>
2024-04-11 17:58:08 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,26 +6,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	int bubble, temp;
+	size_t i, j;
+	int temp;
 
-	if (array == NULL || size < 2)
-		return;
 
-	while (bubble == 0)
+	for (i = 0; i < size - 1; i++)
 	{
-		bubble = 1;
-		for (i = 0; i < len - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
-				bubble = 0;
 			}
 		}
-		len--;
 	}
 }
