c109b03c5cf8cf0a83c706c12dd413c9bea7f8b0
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-04-21 10:48:26 +0000

    error fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+
 /**
  * selection_sort - Calls function
  * @array: Array to be sorted
  * @size: Size of array given
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index;
 	int temp;
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size - d1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
 		for (j = i + 1; j < size; j++)
